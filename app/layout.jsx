0101c099caec54a1dfe8ba4369f8b7b6a0c0dc45
adrian-luque <adrian.luque@perondagroup.com>
2025-08-04 09:00:46 +0200

    fix: layout tittle changes by Traffic Metrics

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 const URL = process.env.NEXT_PUBLIC_URL;
 
 export const metadata = {
-  title: "Peronda Group CRM",
+  title: "Peronda Group Traffic Metrics",
   description: "Created by Peronda Group IT Team",
   icons: {
     icon: `${URL}/favicon.ico`, // Default favicon
